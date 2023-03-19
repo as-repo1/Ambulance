149184a86042b6c3d65781a83a15cad5829c57e8
Abhinaba Sarkar <abhinabasarkar22@gmail.com>
2023-03-19 21:05:06 +0530

    activity update, main.kt update, drawable update , total - 6 added, 4 modified, 2 deleted.

---
+++ app/src/main/java/com/example/ambulance/HomeActivity.java
@@ -0,0 +1,6 @@
+package com.example.ambulance;
+
+import android.app.Activity;
+
+public class HomeActivity extends Activity {
+}
--- app/src/main/java/com/example/ambulance/MainActivity.kt
+++ app/src/main/java/com/example/ambulance/MainActivity.kt
@@ -1,37 +1,43 @@
 package com.example.ambulance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.os.Parcel
+import android.os.Parcelable
+import android.widget.Button
+import com.example.ambulance.R
 
-@Suppress("DEPRECATION")
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity(), Parcelable {
+
+    constructor(parcel: Parcel) : this() {
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        // Get reference to the buttons in the layout
 
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_home -> {
-                    // Handle the Home option click
-                    true
-                }
-                R.id.navigation_hospitals -> {
-                    // Handle the Hospitals option click
-                    true
-                }
-                R.id.navigation_chat -> {
-                    // Handle the Chat with Doctor option click
-                    true
-                }
-                R.id.navigation_profile -> {
-                    // Handle the Profile option click
-                    true
-                }
-                else -> false
-            }
+        // Set click listeners for each button to launch the corresponding activity
+
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
         }
     }
 }
